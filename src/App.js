6d557aa239aba2aedf0013783da57bfa39545b59
Ann-14 <anna.villar34@gmail.com>
2022-11-21 09:59:31 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Escena } from "./components/escena/Escena";
 
 function App() {
+  const HeroStory = [
+    {
+      text: "El nostre heroi estava surant per l'espai sideral quan a la llunyania va albirar una nau espacial",
+    },
+    {
+      text: "Sentia curiositat per l'interior de la nau i es va posar a inspeccionar-la. Va arribar a una sala amb dues portes.",
+    },
+    { text: "L'heroi va decidir travessar la porta que el portava a casa" },
+    {
+      text: "Mentrestant, altres herois no van tenir tanta sort en la seva elecció ...",
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+  <Escena text={HeroStory[0].text} />;
+  <Escena text={HeroStory[1].text} />;
+  <Escena text={HeroStory[2].text} />;
+  <Escena text={HeroStory[3].text} />;
+  </>
+  )
 }
 
 export default App;
